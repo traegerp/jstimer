f4f39f0bfb6cca60e914071359f36e06204899b6
= <=>
2015-09-27 18:55:33 -0700

    fix issue with clear method, stop method and start method

--- jstimer.js
+++ jstimer.js
@@ -8,41 +8,55 @@
 		if(Object.prototype.toString.call(options) === '[object Object]'){
 			this.time 		= options.init || 0;
 			this.enabled 	= false;
-			this.html 		= options.target || null;
+			this.id  		= options.target || null;
 			this.method 	= options.method || 'interval';
+			this.output 	= options.format || 'mmss';
 			return this;			
 		}
 	}
 
 	Timer.prototype.format = function(time){
-		if(Object.prototype.toString.call(time) !== '[object Undefined]'){			
-			return Math.floor(time / 60);
+		if(Object.prototype.toString.call(time) !== '[object Undefined]'){	
+			if(this.output.toLowerCase() == 'mmss'){
+				var min = time / 60;
+				var sec = ('.' + min.toString().split('.')[1]) * 60;
+				if(isNaN(sec)){
+					sec = 0;
+				}
+				return (Math.floor(min) < 10 ? '0' + Math.floor(min) : Math.floor(min)) + ':' + (Math.ceil(sec) < 10 ? '0' + Math.ceil(sec) : Math.ceil(sec));				
+			}		
+			else if(this.format.toLowerCase() == 'mm'){
+				return Math.floor(time / 60);
+			}
 		}
 	};
 
 	Timer.prototype.start = function(){
-		if(this.method === 'interval'){
-			this.enabled = true;
-			var second 	 = 1000;
-			var self 	 = this;
-			if(this.html){
-				self.html = document.getElementById(this.html);
-			}
-			this.id = setInterval(function(){
-				self.time++;
-				if(self.html){
-					self.html.innerHTML = self.format(self.time);
+		if(!this.enabled){
+			if(this.method === 'interval'){
+				this.enabled = true;
+				var second 	 = 1000;
+				var self 	 = this;
+				if(this.id){
+					self.html = document.getElementById(this.id);
 				}
-			}, second);
-		}
-		else{
-			this.startTime = Date.now();
+				this.intervalId = setInterval(function(){
+					self.time++;
+					if(self.html){
+						self.html.innerHTML = self.format(self.time);
+					}
+				}, second);
+			}
+			else{
+				this.startTime = Date.now();
+			}
 		}
 	};
 
 	Timer.prototype.stop = function(){
 		if(this.method === 'interval'){
-			clearInterval(this.id);
+			this.enabled = false;
+			clearInterval(this.intervalId);
 		}
 		else{
 			this.endTime = Date.now();
@@ -58,6 +72,6 @@
 		this.stop();
 		this.time = 0;
 		if(this.html){
-			document.getElementById(this.html).innerHTML = this.time;	
+			this.html.innerHTML = this.format(this.time);
 		}
 	};
